6a34850423a43864310b374af91a0228868a440e
Shaun Porter <shaunoman840@gmail.com>
2019-01-31 14:26:46 +0000

    Uploaded dataset and midway through plotting of data
    Dataset.xlsx contains the dataset we're working with, it requires an import on every re-opening of Matlab.
    
    4dPlot.asv is the file used to plot the dataset. Currently the objective is to plot the four parameters.
    
    sepal length, sepal width and petal length is to be respresented with world cordernates. (X,Y,Z) petalWidth with colour (black for 0, white for 3.5) and for the flower type the shape of the plotted point will differ for each flower respectively.

---
+++ main.m
@@ -0,0 +1,12 @@
+sepalLength = dataset{:,1};
+sepalWidth = dataset{:,2};
+petalLength = dataset{:,3};
+petalWidth = dataset{:,4};
+
+scatter3(sepalLength,sepalWidth,petalLength,40,petalWidth,"filled");
+
+
+
+
+
+
