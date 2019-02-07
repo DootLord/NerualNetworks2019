3c640caec8d0992557ea7140d6657d3e7c867ef4
Shaun Porter <shaunoman840@gmail.com>
2019-02-07 14:52:11 +0000

    Updated graph to have rough legend (WIP)

--- main.m
+++ main.m
@@ -4,9 +4,10 @@ petalLength = dataset{:,3};
 petalWidth = dataset{:,4};
 
 scatter3(sepalLength,sepalWidth,petalLength,40,petalWidth,"filled");
-
-
-
-
-
+title("Iris Dataset");
+xlabel("Sepal Length");
+ylabel("Sepal Width");
+zlabel("Petal Length");
+colorbar('Location', 'EastOutside', 'YTickLabel',...
+{'0.31cm', '0.63cm', '0.94cm', '1.25cm', '1.56cm', '1.88cm', '2.19cm', '2.5cm'})
 
