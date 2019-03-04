47bbe8ebeefc06ea680ffce28d45a18192f5db14
Shaun Porter <shaunoman840@gmail.com>
2019-03-04 15:04:32 +0000

    Nerual Network Completed.
    Spreadsheet with results added
    Iris Network saved as a .mat file.

--- guiAttempt.m
+++ guiAttempt.m
@@ -1,2 +1,17 @@
-XORinputT = XORinput';
-XORtargetsT = XORtargets';
+data = xlsread("datasetProcessed.xlsx");
+testData = xlsread("datasetTraining.xlsx");
+
+input = data(:,1:4);
+output = data(:,5:7);
+testing = testData(:,1:4);
+
+inputT = input';
+outputT = output';
+testingT = testing';
+
+
+
+
+
+
+
